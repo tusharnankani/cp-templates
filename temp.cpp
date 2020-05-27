eb058983f047c357954dec0168ea4acc1b0cf892
Tushar Nankani <tusharnankani3@gmail.com>
2020-05-27 22:18:26 +0530

    Create temp.cpp
    Template for Competitive Programming

---
+++ temp.cpp
@@ -0,0 +1,38 @@
+#pragma GCC optimize("O2") 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define test(t)		int t; cin>>t; while(t--)
+#define f(i, a, b)	for(int i = a; i < b; ++i)
+#define all(a)		a.begin(), a.end()
+#define endl 		"\n"
+#define deb(x)		cout << #x << "--" << x << "--" << endl;
+#define deb2(x,y)	cout << #x << "-" << x << "-" << " ~~ " <<  #y << "-" << y << "-" << endl;
+#define deb3(x,y,z)	cout << #x << "-" << x << "-" << " ~~ " <<  #y << "-" << y << "-" << " ~~ " << #z << "-" << z << "-" << endl;
+#define deba(arr)	cout << #arr << " ~ [ "; for(auto n: arr) cout << n << " "; cout << "]" << endl;
+#define fast 		ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define mx9		1000000007
+#define mx7		10000007
+#define mx6		1000006
+#define mx5		200005
+#define inf		1<<30                                 
+#define eps		1e-9
+#define mod		1000000007
+#define PI		3.141592653589793238462643383279502884L
+
+void solve()
+{
+}
+
+int main()
+{
+    fast;
+    test(t)
+        solve();
+    return 0;
+}
