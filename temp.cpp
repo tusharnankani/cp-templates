bb0492089416380860803253b4b7a9e0fe378392
Tushar Nankani <tusharnankani3@gmail.com>
2020-06-25 20:00:27 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -14,19 +14,19 @@ typedef vector<ll>              vll;
 typedef vector<pii>             vpii;
 typedef vector<pll>             vpll;
 
-#define test(t)		int t; cin >> t; while(t--)
-#define f(i, a, b)	for(int (i) = (a); (i) < (b); ++(i))
-#define all(a)		(a).begin(), (a).end()
-#define endl 		"\n"
-#define maxvec(vec)     *max_element(all(vec))
-#define minvec(vec)     *min_element(all(vec))
-#define maxarr(arr)     *max_element(arr, arr + n)
-#define minarr(arr)     *min_element(arr, arr + n)
-#define deb(x)		cout << #x << "--" << x << "--" << endl;
-#define deb2(x,y)	cout << #x << "-" << x << "-" << " ~~ " <<  #y << "-" << y << "-" << endl;
-#define deba(arr)	cout << #arr << " ~ [ "; for(auto n: arr) cout << n << " "; cout << "]" << endl;
-#define debap(arr)	cout << #arr << " ~ [ "; for(auto n: arr) cout << n.first << "-" << n.second << " "; cout << "]" << endl;
-#define fast 		ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define test(t)		        int t; cin >> t; while(t--)
+#define f(i, a, b)	        for(int (i) = (a); (i) < (b); ++(i))
+#define all(a)		        (a).begin(), (a).end()
+#define endl 		        "\n"
+#define maxvec(vec)             *max_element(all(vec))
+#define minvec(vec)             *min_element(all(vec))
+#define maxarr(arr)             *max_element(arr, arr + n)
+#define minarr(arr)             *min_element(arr, arr + n)
+#define deb(x)		        cout << #x << "--" << x << "--" << endl;
+#define deb2(x,y)	        cout << #x << "-" << x << "-" << " ~~ " <<  #y << "-" << y << "-" << endl;
+#define deba(arr)	        cout << #arr << " ~ [ "; for(auto n: arr) cout << n << " "; cout << "]" << endl;
+#define debap(arr)	        cout << #arr << " ~ [ "; for(auto n: arr) cout << n.first << "-" << n.second << " "; cout << "]" << endl;
+#define fast 		        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 #define mx9		1000000007
 #define mx7		10000007
