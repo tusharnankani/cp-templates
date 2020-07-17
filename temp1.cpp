23c5dc7d5c28551dcf31b1a75efbef5fd1e234c3
Tushar Nankani <tusharnankani3@gmail.com>
2020-07-18 00:34:17 +0530

    Update temp1.cpp

--- temp1.cpp
+++ temp1.cpp
@@ -3,13 +3,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int           ll;
+typedef long long int ll;
 
-#define test(t)		        int t; cin >> t; while(t--)
-#define f(i, a, b)	        for(int (i) = (a); (i) < (b); ++(i))
-#define endl 		        "\n"
-#define deb(x)		        cout << #x << ": " << x << endl;
-#define fast 		        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define test(t) int t; cin >> t; while(t--)
+#define f(i, a, b) for(int (i) = (a); (i) < (b); ++(i))
+#define endl "\n"
+#define deb(x) cout << #x << ": " << x << endl;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 void solve()
 {
