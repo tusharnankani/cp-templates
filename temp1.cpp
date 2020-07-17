ae1b83af2d98194a03e87e130fb9bf27a38b74c5
Tushar Nankani <tusharnankani3@gmail.com>
2020-07-17 23:12:02 +0530

    Create temp1.cpp

---
+++ temp1.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize("O3") 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int           ll;
+
+#define test(t)		        int t; cin >> t; while(t--)
+#define f(i, a, b)	        for(int (i) = (a); (i) < (b); ++(i))
+#define endl 		        "\n"
+#define deb(x)		        cout << #x << ": " << x << endl;
+#define fast 		        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve()
+{
+}
+
+int main()
+{
+    fast;
+    test(t)
+        solve();
+    return 0;
+}
