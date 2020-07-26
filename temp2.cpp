29ed787e0a4a85ce7252943071cad887514b3153
Tushar Nankani <tusharnankani3@gmail.com>
2020-07-26 20:09:59 +0530

    Update temp2.cpp

--- temp2.cpp
+++ temp2.cpp
@@ -7,15 +7,15 @@ typedef long long int           ll;
 typedef unsigned long long int  ull;
 typedef long double             ld;
 
-#define test(t)		        int t; cin >> t; while(t--)
-#define f(i, a, b)	        for(int (i) = (a); (i) < (b); ++(i))
-#define all(a)		        (a).begin(), (a).end()
-#define endl 		        "\n"
-#define deb(x)		        cout << #x << ": " << x << endl;
-#define deb2(x,y)	        cout << #x << ": " << x << " ~ " <<  #y << ": " << y << endl;
-#define deba(arr)	        cout << #arr << " ~ [ "; for(auto n: arr) cout << n << " "; cout << "]" << endl;
-#define debap(arr)	        cout << #arr << " ~ [ "; for(auto n: arr) cout << n.first << "-" << n.second << " "; cout << "]" << endl;
-#define fast 		        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define test(t)		    int t; cin >> t; while(t--)
+#define f(i, a, b)	    for(int (i) = (a); (i) < (b); ++(i))
+#define all(a)		    (a).begin(), (a).end()
+#define endl 		    "\n"
+#define deb(x)		    cout << #x << ": " << x << endl;
+#define deb2(x,y)	    cout << #x << ": " << x << " ~ " <<  #y << ": " << y << endl;
+#define deba(arr)	    cout << #arr << " ~ [ "; for(auto n: arr) cout << n << " "; cout << "]" << endl;
+#define debap(arr)	    cout << #arr << " ~ [ "; for(auto n: arr) cout << n.first << "-" << n.second << " "; cout << "]" << endl;
+#define fast 		    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 #define mod		1000000007
 #define mx7		10000007
