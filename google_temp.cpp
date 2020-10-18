34ea96bfe2ec62843cd93b47c134369a37d1d44f
Tushar Nankani <tusharnankani3@gmail.com>
2020-10-18 18:38:38 +0530

    Create google_temp.cpp

---
+++ google_temp.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("O3") 
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+
+using namespace std;
+using namespace __gnu_pbds; 
+
+typedef long long int           ll;
+typedef unsigned long long int  ull;
+typedef long double             ld;
+
+typedef pair<int, int>          pii;
+typedef pair<ll,ll>             pll;
+typedef vector<int>             vi;
+typedef vector<ll>              vll;
+typedef vector<pii>             vpii;
+typedef vector<pll>             vpll;
+
+#define f(i, a, b)	        for(int (i) = (a); (i) < (b); ++(i))
+#define all(a)		        (a).begin(), (a).end()
+#define endl 		          "\n"
+#define deb(x)		        cout << #x << ": " << x << endl;
+#define deb2(x,y)	        cout << #x << ": " << x << " ~ " <<  #y << ": " << y << endl;
+#define deba(arr)	        cout << #arr << " ~ [ "; for(auto n: arr) cout << n << " "; cout << "]" << endl;
+#define debap(arr)	        cout << #arr << " ~ [ "; for(auto n: arr) cout << n.first << "-" << n.second << " "; cout << "]" << endl;
+#define fast 		        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define mx9		1000000007
+#define mx7		10000007
+#define mx6		1000006
+#define mx5		200005
+#define inf		1<<30                                 
+#define eps		1e-9
+#define mod		1000000007
+#define PI		3.141592653589793238462643383279502884L
+
+void solve()
+{
+}
+
+int main()
+{
+    fast;
+    ll t; cin >> t;
+    ll n = t;
+    while(t--)
+    {
+        cout << "Case #" << n - t << ": ";
+        solve();
+    }
+    return 0;
+}
